6f71ef644bfdda3b334884a5d0b83564abf388e9
Naoki Hatta <naohatta69@gmail.com>
2016-01-10 13:50:38 +0900

    fill circle corresponding to count

--- public/javascripts/drawCanvas.js
+++ public/javascripts/drawCanvas.js
@@ -1,12 +1,25 @@
 onload = function(){
-    drawCounter();      
+    drawCounterBase();      
 }
 
-function drawCounter(){
+
+function drawCounterBase(){
    var cnvs = document.getElementById('canvas-counter');
    var ctx = cnvs.getContext('2d');
    ctx.beginPath();
-   ctx.arc(100, 105, 100, 0, Math.PI*2, false);
+   ctx.arc(110, 110, 100, 0, Math.PI*2, false);
    ctx.strokeStyle = 'green';
    ctx.stroke();
+}
+
+function updateCounter(sum){
+    var cnvs = document.getElementById('canvas-counter');
+    var ctx = cnvs.getContext('2d');
+    var maxSum = 15;
+    ctx.beginPath();
+    ctx.moveTo(110,110);
+    ctx.arc(110, 110, 100, -Math.PI*90/180, -Math.PI*90/180 + Math.PI*sum/maxSum, false);
+    ctx.closePath();
+    ctx.fillStyle = 'red';
+    ctx.fill();
 }
