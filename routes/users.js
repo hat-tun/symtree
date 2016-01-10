0386cead5f664752881e248e33cbcd873dfa35d8
Naoki Hatta <naohatta69@gmail.com>
2016-01-10 11:27:56 +0900

    create form to post counter data

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('users', { title: 'User Page' });
 });
 
+router.post('/', function(req, res, next) {
+    console.log(req.body);
+});
+
 module.exports = router;
