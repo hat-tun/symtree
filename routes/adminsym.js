f8fb9c955f64b05e4f2182c0cfd6f7ae5acf0ee5
Naoki Hatta <naohatta69@gmail.com>
2016-02-25 11:53:37 +0900

    add admin page

---
+++ routes/adminsym.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('adminSym', { total: total_counter });
+});
+
+router.post('/', function(req, res, next) {
+    console.log(req.body);
+    if(req.body.totalReset == '0')
+    {
+	total_counter = 0;
+    }
+});
+
+module.exports = router;
